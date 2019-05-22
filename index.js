677cc0fed914486030cd90401c79eea72573d29d
Matt Milliron <32225206+mmilliron@users.noreply.github.com>
2019-05-22 18:26:26 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+'use strict';
+
+// put your own value below!
+const searchURL = 'https://www.anapioficeandfire.com/api/characters/';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+
+function getCharacter(query) {
+  const params = {
+    q: query,
+    part: 'snippet',
+    
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    getCharacter(searchTerm,);
+  });
+}
+
+$(watchForm);
